57fa328df337abeca6371755dceffe1edd76ee79
kabboCSE <shahriar15-10494@diu.edu.bd>
2024-12-26 02:55:06 +0600

    git init

---
+++ src/main/java/java_learn/DataTypes.java
@@ -0,0 +1,27 @@
+package java_learn;
+
+public class DataTypes {
+
+    public static void main (String[] args) {
+        // Primitive Data Types
+        // Syntax: variable_type variable_name = variable_value
+
+        int number = 100;
+        float number2 = 10.45F;
+        double number3 = 24353533;
+        char letter = 'a';
+        boolean bool = true;
+
+        System.out.println("The Number is: "+ number);
+        System.out.println("The Number is: "+ number+", Float is: "+ number2);
+
+        //Non Primitive data types
+
+        String name = "Shahriar";
+        int[] num = {1,2,3,4,5};
+
+        System.out.println("The Name is: "+ name);
+        System.out.println("The Number is: "+ num[2]);
+
+    }
+}
---
+++ src/main/java/java_learn/Variables.java
@@ -0,0 +1,11 @@
+package java_learn;
+
+public class Variables {
+    String str = "Kabbo";
+    public static void main(String[] args) {
+        int var = 20;
+        System.out.println("var = " + var);
+       Variables obj = new Variables();
+        System.out.println(obj.str);
+    }
+}
